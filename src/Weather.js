1c063c39d27360523b6d58da0e49976c77fa1b3d
MaVeRohoho <76155685+MaVeRohoho@users.noreply.github.com>
2021-02-16 12:43:01 +0100

    fixed date function

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import DateFormat from "./DateFormat";
 import './Weather.css';
 import React, {useState} from "react";
 import axios from "axios"; 
 
 export default function Weather(props) {
-let [weather, setWeather] = useState({ready: false});
+const [weather, setWeather] = useState({ready: false});
 
 function handleResponse (response){
   setWeather({
       ready: true,
       temperature: response.data.main.temp,
       city: response.data.name,
-      date: new Date(response.data.coord.dt *1000),
+      date: new Date(response.data.dt *1000),
       description: response.data.weather[0].description,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
@@ -65,7 +66,7 @@ if (weather.ready){
                 <div className="row">
                     <div className="col-6">
                         <ul>
-                            <li id="date">Date: <DateFormat date = {weather.date}/></li>
+                            <li id="date"><DateFormat date = {weather.date}/></li>
                             <li id="description">{weather.description}</li>
                         </ul>
                     </div>
