fad7fe1dfc7264a30ebf94c9ed481eda3f101377
MaVeRohoho <76155685+MaVeRohoho@users.noreply.github.com>
2021-02-20 15:43:24 +0100

    updated FontAwesome Icons importing

--- src/Icons.js
+++ src/Icons.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fal } from '@fortawesome/pro-light-svg-icons'
 
 library.add(fal)
 
 export default function Icons {
-<i class="fal fa-sun"></i>
-    
-}
- (
+const Showcase = () => (
   <div>
-    <FontAwesomeIcon icon="sun" /> 
-  </div>
-)
+    <FontAwesomeIcon icon={['fal', 'sun']} />
+    <FontAwesomeIcon icon={['fal', 'moon-stars']} />
+    <FontAwesomeIcon icon={['fal', 'cloud-sun']} />
+    <FontAwesomeIcon icon={['fal', 'moon-cloud']} />
+    <FontAwesomeIcon icon={['fal', 'cloud']} />
+    <FontAwesomeIcon icon={['fal', 'clouds-sun']} />
+    <FontAwesomeIcon icon={['fal', 'clouds-moon']} />
+    <FontAwesomeIcon icon={['fal', 'cloud-moon-rain']} />
+    <FontAwesomeIcon icon={['fal', 'cloud-showers-heavy']} />
+    <FontAwesomeIcon icon={['fal', 'cloud-sun-rain']} />
+    <FontAwesomeIcon icon={['fal', 'cloud-moon-rain']} />
+    <FontAwesomeIcon icon={['fal', 'thunderstorm-sun']} />
+    <FontAwesomeIcon icon={['fal', 'thunderstorm-moon']} />
+    <FontAwesomeIcon icon={['fal', 'snowflake']} />
+    <FontAwesomeIcon icon={['fal', 'snowflakes']} />
+    <FontAwesomeIcon icon={['fal', 'fog']} />
+</div>
+);
+}
