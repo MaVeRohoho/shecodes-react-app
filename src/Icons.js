3e0bd8069fe3bdfddc5b000c5b26157d1134d6e1
MaVeRohoho <76155685+MaVeRohoho@users.noreply.github.com>
2021-02-20 12:10:40 +0100

    updated icons.js and installed fontawesome.

--- src/Icons.js
+++ src/Icons.js
@@ -1,23 +1,11 @@
-//import React from 'react'
-//import FontAwesome from 'react-fontawesome'
-//import faStyles from 'font-awesome/css/font-awesome.css'
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+export function Icon(props) {
+    
+  return <i classname="fa"></i>
+}
  
-//var MyComponent = React.createClass({
- // render: function() {
-   // return (
-   //   <FontAwesome
-    //    className="super-crazy-colors"
-   //     name="rocket"
-   //     cssModule={faStyles}
-  //      size="2x"
-    //    spin
-    //    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
- //     />
-//   )
- // },
-//})
-
-
-//`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 
 
